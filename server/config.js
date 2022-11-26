402627e886cd5449edf26df7062b0f11e2a2a75f
nardo88 <nardo1988@mail.ru>
2022-11-26 22:32:33 +0300

    add

---
+++ server/config.js
@@ -0,0 +1,5 @@
+// разрешенный источник
+export const ALLOWED_ORIGIN = "http://localhost:3000";
+// адрес БД
+export const MONGODB_URI =
+  "mongodb+srv://admin:admin@cluster0.rwelf.mongodb.net/socketRoom?authSource=admin&replicaSet=atlas-xyu5vf-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
